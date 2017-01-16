eed348d0c97bc8f22988aad56c821a94438af42b
ttjnguyen <ttjnguyen6@gmail.com>
2017-01-16 09:41:23 -0800

    final commits

---
+++ TechInterviewLHL/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TechInterviewLHL
+//
+//  Created by Jenny Nguyen on 2016-12-22.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TechInterviewLHL/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TechInterviewLHL
+//
+//  Created by Jenny Nguyen on 2016-12-22.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
